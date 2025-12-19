21b7f056871732c97cc11d9991793eaeb58e4537
刘怡豪 <mildwind2022@gmail.com>
2025-12-19 16:11:34 +0800

    更新隐藏Bug

--- Game/scene2.cpp
+++ Game/scene2.cpp
@@ -166,9 +166,8 @@ void CheckCollision_GameScene_Player_Enemies()
     rect1.top = player->position.y;
     rect1.bottom = player->position.y + player->height;
     // 敌人用简单矩形表示
-    std::vector<Enemy *> enemies = GetEnemies();
     Rect rect2{};
-    for (Enemy *enemy : enemies)
+    for (Enemy *enemy : GetEnemies())
     {
         rect2.left = enemy->position.x;
         rect2.right = enemy->position.x + enemy->width;
@@ -193,18 +192,16 @@ void CheckCollision_GameScene_Player_Enemies()
 void CheckCollision_GameScene_Enemies_Bullets()
 {
     // 敌人用简单矩形表示
-    std::vector<Enemy *> enemies = GetEnemies();
     Rect rect{};
     // 子弹用简单圆形表示
-    std::vector<Bullet *> bullets = GetBullets();
     Circle circle{};
-    for (Enemy *enemy : enemies)
+    for (Enemy *enemy : GetEnemies())
     {
         rect.left = enemy->position.x;
         rect.right = enemy->position.x + enemy->width;
         rect.top = enemy->position.y;
         rect.bottom = enemy->position.y + enemy->height;
-        for (Bullet *bullet : bullets)
+        for (Bullet *bullet : GetBullets())
         {
             circle.center = bullet->position;
             circle.radius = bullet->radius;
